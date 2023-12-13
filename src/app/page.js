a1be99f680aa7c311594b8e6b30d7f42d31b8794
Oziel Guerra <ozi_guerra@hotmail.com>
2023-12-13 09:45:29 -0800

    Added location info, contact us directly button

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="absolute inset-0 w-full h-screen bg-slate-100 bg-opacity-60"></div>
 
         <div className="flex h-full items-center justify-center">
-          <div className="px-6 text-center text-black z-10 md:px-12">
+          <div className="px-6 flex flex-col text-center items-center justify-center text-black z-10 md:px-12">
             <h1 className="mt-2 mb-16 text-4xl font-semibold font-kalnia tracking-tight md:text-5xl xl:text-6xl">
               Mazatlan 2503, <br />
               <span>Your home, away from home</span>
@@ -18,11 +18,19 @@ export default function Home() {
             <a
               href="https://www.airbnb.mx/rooms/48401709"
               target="_blank"
-              className="rounded border-2 border-black z-10 px-[46px] pt-[14px] pb-[12px] font-kalnia text-sm font-medium uppercase leading-normal text-black transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-10 hover:text-neutral-100"
+              className="rounded border-2 border-black text-center z-10 m-2 px-[46px] pt-[14px] pb-[12px] font-kalnia text-sm font-medium uppercase leading-normal text-black transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-10 hover:text-neutral-100"
               data-te-ripple-init
               data-te-ripple-color="light"
             >
               Book on Airbnb
+            </a>
+            <a
+              href="mailto:mazatlan2503@gmail.com"
+              className="rounded border-2 border-black text-center z-10 m-2 px-[46px] pt-[14px] pb-[12px] font-kalnia text-sm font-medium uppercase leading-normal text-black transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-10 hover:text-neutral-100"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              Contact us directly
             </a>
           </div>
         </div>
@@ -35,6 +43,11 @@ export default function Home() {
           <Amenity key={amenity.title} {...amenity} />
         ))}
         </div>
+      </div>
+
+      <div className="flex flex-col w-full items-center mb-4">
+      <h1 className="p-9 font-kalnia text-xl xl:text-4xl">Location</h1>
+      <iframe className="border-double border-4 border-black" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.9332126144614!2d-106.45608522367115!3d23.245517307969752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x869f550b9a421661%3A0x48686ce8b59158ad!2sCamino%20al%20Mar%20-%20Plaza%20Comercial!5e0!3m2!1sen!2sca!4v1702489121270!5m2!1sen!2sca" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
       {/* Footer */}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="h-20 md:h-22 w-full bg-sky-200 flex justify-center items-center">
+    <div className="h-20 md:h-22 w-full bg-sky-200 flex justify-center items-center border-t-2 border-black">
       {/*<a href="https://github.com/oziguerra" target="_blank" className="m-6 md:m-8 text-primary"><IoLogoGithub className="w-auto h-9"/></a>  
   <a href="https://www.linkedin.com/in/ozielguerra/" target="_blank" className="m-6 md:m-8 text-primary"><FaLinkedinIn className="w-auto h-9" /></a>*/}
       <p className=" text-slate-800 font-bold"> Mazatlan 2503 </p>
